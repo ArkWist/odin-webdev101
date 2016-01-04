9e6764314d808712462cd0cd77994f4e15589395
Daniel A. G. Lilley <arkwist@gmail.com>
2016-01-04 18:24:02 +0900

    Functional prototype complete.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,52 @@
 
 $(document).ready(function(){
 
-	var controlPanel = '<div id="control-panel"></div>';
-	$("body").prepend(controlPanel);
-	var controlButton = '<button type="button">Restart</button>';
-	$("#control-panel").append(controlButton);
+	var gridWidth = 16;
+	var gridHeight = 16;
+	var containerDimension = 600;
+	var cellOverflowCorrection = .001;
 
-	var gridW = 16;
-	var gridH = 16;
-
-	for(var i = 0; i < gridH; i++){
-		var row = document.createElement("div");
-		row.className = "grid-row";
-		for(var j = 0; j < gridW; j++){
-			var cell = document.createElement("div");
-			cell.className = "grid-cell";
-			$(row).append(cell);
+	// Create cells that fit within the grid container dimensions.
+	var createGrid = function(gridWidth, gridHeight){
+		var cellWidth = containerDimension / gridWidth - cellOverflowCorrection;
+		var cellHeight = containerDimension / gridHeight - cellOverflowCorrection;
+		console.log("gridWidth: "+gridWidth+", gridHeight: "+gridHeight);
+		console.log("cellWidth: "+cellWidth+", cellHeight: "+cellHeight);
+		for(var i = 0; i < gridHeight; i++){
+			var row = document.createElement("div");
+			row.className = "grid-row";
+			for(var j = 0; j < gridWidth; j++){
+				var cell = document.createElement("div");
+				cell.className = "grid-cell";
+				$(cell).css({"width":cellWidth, "height":cellHeight});
+				$(row).append(cell);
+			}
+			$("#grid-container").append(row);
 		}
-		$("#grid-container").append(row);
-	}
+	};
 
-	$(".grid-cell").hover(function(){
+	var clearGrid = function(){
+		$("#grid-container").empty();
+	};
+
+	$(document).on('click', '#new-grid', function(){
+		gridWidth = prompt("Enter horizontal cell count");
+		gridHeight = prompt("Enter vertical cell count");
+		clearGrid();
+		createGrid(gridWidth, gridHeight);
+	});
+
+	$(document).on('mouseover', '.grid-cell', function(){
 		$(this).addClass("grid-hover");
 	});
+
+	// Create a starter grid and New Grid controls.
+	var controlPanel = '<div id="control-panel"></div>';
+	$("body").prepend(controlPanel);
+	var controlButton = '<button type="button" id="new-grid">New Grid</button>';
+	$("#control-panel").append(controlButton);
+	$("#grid-container").css({"width":containerDimension, "height":containerDimension});
+	createGrid(gridWidth, gridHeight);
 
 });
 
