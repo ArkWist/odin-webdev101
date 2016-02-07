f7b1776cefb18cb6c8bb2dd47561f97df84e2b6e
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-07 10:25:36 +0900

    Add index.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+Blogger::Application.routes.draw do
+  resources :articles
+end
+
+#Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+#end
